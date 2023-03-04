8fa5bb506f6ab51124e44184191d9b8b111af3f4
Julius A <julius.bichage@gmail.com>
2023-03-04 21:38:49 +0000

    few tweaks

--- src/Project/horizon/src/Pages/Work.jsx
+++ src/Project/horizon/src/Pages/Work.jsx
@@ -13,9 +13,10 @@ const Work = () => {
                 <Card style={{ width: '100%' }}>
                     <Card.Body className='text-center'>
                         <h2>Work Orders</h2>
+                        <Translate/>
                     </Card.Body>
                 </Card>
-                <Translate/>
+                
             </header>
 
             <div className="full-container d-flex text-center justify-content-center  p-2">
