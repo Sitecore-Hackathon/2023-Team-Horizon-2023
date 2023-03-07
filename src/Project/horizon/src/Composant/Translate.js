6346a7e2a4dc550fca9a6c660af74eb7dd1237bc
souhir.bouaicha <souhir.bouaicha@outlook.com>
2023-03-07 21:44:29 +0100

    call translate on change textarea selector lang

--- src/Project/horizon/src/Composant/Translate.js
+++ src/Project/horizon/src/Composant/Translate.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import {Form,TextArea } from 'semantic-ui-react';
+import { Form, TextArea } from 'semantic-ui-react';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import openAI from '../config';
 
-const Translate = () =>  {
+const Translate = () => {
     const [inputText, setInputText] = useState('');
     const [resultText, setResultText] = useState('');
     const [selectedLanguageKey, setLanguageKey] = useState('')
@@ -15,9 +15,9 @@ const Translate = () =>  {
         axios.post(`${process.env.REACT_APP_LIBRE_BASEURL}/detect`, {
             q: inputText
         })
-        .then((response) => {
-            setdetectedLanguageKey(response.data[0].language)
-        })
+            .then((response) => {
+                setdetectedLanguageKey(response.data[0].language)
+            })
     }
     const libreTranslateText = () => {
         setResultText(inputText)
@@ -25,17 +25,17 @@ const Translate = () =>  {
         getLanguageSource();
 
         let data = {
-            q : inputText,
+            q: inputText,
             source: detectLanguageKey,
             target: selectedLanguageKey
         }
         axios.post(`${process.env.REACT_APP_LIBRE_BASEURL}/translate`, data)
-        .then((response) => {
-            setResultText(response.data.translatedText)
-        })
+            .then((response) => {
+                setResultText(response.data.translatedText)
+            })
     }
 
-    const openAiTranslate = async () => {        
+    const openAiTranslate = async () => {
         const completion = await openAI.createCompletion({
             model: "text-davinci-003",
             prompt: `translate ${inputText}?? in ${selectedLanguageName}`,
@@ -47,52 +47,58 @@ const Translate = () =>  {
     }
 
     const handleLanguageSelection = (selectedLanguage) => {
+        openAiTranslate()
         setLanguageName(selectedLanguage.target.value);
+
     }
 
     useEffect(() => {
-       axios.get(`${process.env.REACT_APP_LIBRE_BASEURL}/languages`)
-       .then((response) => {
-        setLanguagesList(response.data)
-       })
+        axios.get(`${process.env.REACT_APP_LIBRE_BASEURL}/languages`)
+            .then((response) => {
+                setLanguagesList(response.data)
+            })
 
-       getLanguageSource()
+        getLanguageSource()
     }, [inputText])
     return (
         <div>
             <div className="app-header">
-                <h4 className="header">Translate from</h4>
+                <h1 className="header mb-3">Translate from</h1>
             </div>
 
             <div className='app-body'>
-                <div>
-                    <Form>
+                <div >
+                    <Form className='d-flex justify-content-center align-items-center w-75 p-4 m-auto bg-light rounded'>
                         <Form.Field
                             control={TextArea}
                             placeholder='Original copy text to translate..'
-                            onChange={(e) => setInputText(e.target.value)}
+                            onChange={(e) => { setInputText(e.target.value); openAiTranslate() }}
+                            className="defaultField m-4"
                         />
+                        <div className='d-flex flex-column'>
+                            <select className="language-select m-3" onChange={handleLanguageSelection}>
+                            {/* onChange={()=>{handleLanguageSelection() ; openAiTranslate()}} */}
 
-                        <select className="language-select" onChange={handleLanguageSelection}>
-                            <option>Translate to?</option>
-                            {languagesList.map((language) => {
-                                return (
-                                    <option value={language.name}>
-                                        {language.name}
-                                    </option>
-                                )
-                            })}
-                        </select>
+                                <option>Translate to?</option>
+                                {languagesList.map((language) => {
+                                    <option value="English">English</option>
+                                    return (
+                                        <option value={language.name}>
+                                            {language.name}
+                                        </option>
+                                    )
+                                })}
+                            </select>
 
-                        <Form.Field
-                            control={TextArea}
-                            placeholder='Output translated text will go here'
-                            value={resultText}
-                        />
+                            <Form.Field
+                                control={TextArea}
+                                placeholder='Output translated text will go here'
+                                value={resultText}
+                            /></div>
 
-                        <Button variant="outline-light" onClick={openAiTranslate} >
+                        {/* <Button variant="primary" onClick={openAiTranslate} className="m-2" >
                             Translate
-                        </Button>
+                        </Button> */}
                     </Form>
                 </div>
             </div>
